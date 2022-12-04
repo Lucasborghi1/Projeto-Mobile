d3b817a6c276a62ea602e1856f8370d3dc70401e
Lucasborghi1 <112809360+Lucasborghi1@users.noreply.github.com>
2022-12-04 14:05:01 -0300

    Update FormAviso.java

--- app/src/main/java/com/example/projetomobile/FormAviso.java
+++ app/src/main/java/com/example/projetomobile/FormAviso.java
@@ -10,6 +10,7 @@ public class FormAviso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_aviso);
+
         getSupportActionBar().hide();
     }
 }
